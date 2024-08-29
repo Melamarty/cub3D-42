7838a3e7a94ddbc71044f26e83314915be8a9c66
Mohamed El-Amarty <mel-amar@e3r2p5.1337.ma>
2024-08-29 19:36:59 +0100

    render

--- parsing/render.c
+++ parsing/render.c
@@ -1,19 +1,19 @@
 #include "parsing.h"
 
-void put_texture(t_map *map)
-{
-	int mod = 8;
-    for (int i = 0; i < map->width * cube_width; i++)
-    {
-        for(int j = 0; j < map->height * cube_width; j++)
-        {
-            if (i % mod && j % mod)
-				mlx_put_pixel(map->img, i, j, create_color(0, 0, 255, 255));
-            else
-                mlx_put_pixel(map->img, i, j, create_color(0, 0, 0, 255));
-        }
-    }
-}
+//void put_texture(t_map *map)
+//{
+//	int mod = 8;
+//    for (int i = 0; i < map->width * cube_width; i++)
+//    {
+//        for(int j = 0; j < map->height * cube_width; j++)
+//        {
+//            if (i % mod && j % mod)
+//				mlx_put_pixel(map->img, i, j, create_color(0, 0, 255, 255));
+//            else
+//                mlx_put_pixel(map->img, i, j, create_color(0, 0, 0, 255));
+//        }
+//    }
+//}
 
 void rect(t_map *map, int x, int y, int width, int height)
 {
@@ -25,11 +25,13 @@ void rect(t_map *map, int x, int y, int width, int height)
 		}
 	}
 }
+
+
 void render(t_map *map)
 {
 	the_rays *ray;
-	double wallWidth = 1;
-	double walHight = 400;
+	int wallWidth = 1;
+	double walHight = 500;
 	//mlx_clear_window(map->mlx, map->we);
 	//put_texture(map);
 	//mlx_put_image_to_window(map->mlx, map->we, map->img, 0, 0);
@@ -41,8 +43,7 @@ void render(t_map *map)
 		raydis = ray->ray_dis;
 		int distoplan = (map->width * cube_width / 2) / (tan(map->player->fov / 2));
 		int wallH = 60 * distoplan / raydis;
-		printf (" %d ", 60 * distoplan / raydis );
-		int y = (map->height * cube_width/ 2) - (walHight / 2);
+		int y = (map->height * cube_width / 2) - (cube_width / 2);
 		rect(map, i * wallWidth, y, wallWidth, wallH);
 		i++;
 		map->rays = map->rays->next;
