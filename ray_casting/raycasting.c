0643144cecfe63b2de73015af5e057c4978835e2
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-09-01 15:06:51 +0100

    minimap done

--- ray_casting/raycasting.c
+++ ray_casting/raycasting.c
@@ -227,7 +227,9 @@ void render_map(t_map *map)
 		yStart = yStartSave;
 		for (int j = 0; j <  6 * m_cube; j++)
 		{
-			if (map->arr[yStart / m_cube][xStart / m_cube] == '1')
+			if (yStart < 0 || xStart < 0)
+				color = create_color(0, 0, 0, 255);
+			else if (map->arr[yStart / m_cube][xStart / m_cube] == '1')
 				color = create_color(0, 100, 0, 255);
 			else
 				color = create_color(144, 238, 144, 255);
