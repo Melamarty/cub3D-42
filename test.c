b9d4d725e1ccd1c67e5b39cc3f069a1e9b317851
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-09-16 11:52:39 +0100

    1337

--- test.c
+++
@@ -1,8 +0,0 @@
-#include <libc.h>
-#include <math.h>
-int main()
-{
-	printf("%lf\n", tan(179 * M_PI / 180));
-	// printf("%lf\n", (3 * M_PI / 2 + (M_PI / 3)) * 180 / M_PI);
-	// printf("%lf\n", fmod(((19 * M_PI / 2) * 180 / M_PI),  (2 * M_PI) * 180 / M_PI));
-}
